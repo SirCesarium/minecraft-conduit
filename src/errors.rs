a754182394b3e6f0719c7c5165ac97f9a750fcc1
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-07-01 19:29:26 -0400

    feat(errors): add custom error variants and InvalidArgument

--- src/errors.rs
+++ src/errors.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::path::PathBuf;
 use std::result;
 
+use minecraft_registry_api::error::ApiError;
 use thiserror::Error;
 use toml::de;
 use toml::ser;
@@ -30,4 +31,16 @@ pub enum Error {
     },
     #[error("failed to serialize TOML")]
     TomlSerialize(#[from] ser::Error),
+    #[error("this source does not support search")]
+    SearchNotSupported,
+    #[error("api error: {0}")]
+    Api(#[from] ApiError),
+    #[error("addon not found: {0}")]
+    AddonNotFound(String),
+    #[error("http error: {0}")]
+    Http(#[from] reqwest::Error),
+    #[error("io error: {0}")]
+    Io(#[from] io::Error),
+    #[error("{0}")]
+    InvalidArgument(String),
 }
